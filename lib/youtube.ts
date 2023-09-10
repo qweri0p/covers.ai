3e5ebe54085ff022df622d97d0f3a409ec89c193
qweri0p <71005650+qweri0p@users.noreply.github.com>
2023-09-10 10:59:47 +0200

    quick little improvement

--- lib/youtube.ts
+++ lib/youtube.ts
@@ -5,10 +5,10 @@ const downloader = new YTDlpWrap()
 export async function getYoutube(link:string, userid: string) {
 
     const metadata = await downloader.getVideoInfo(link)
-    downloader
-        .exec([link, '-x', '-o', `temp/${userid}`])
+    await downloader
+        .execPromise([link, '-x', '-o', `temp/${userid}`])
 
-        .on('close', () => console.log(`Downloaded ${link} (${metadata.title}) to temp/${userid}.`));
+        .finally(() => console.log(`Downloaded ${link} (${metadata.title}) to temp/${userid}.`));
     
     return metadata
 }
